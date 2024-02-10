810a46eb0912e2131683891919218360d7d867f2
OlgaAllirina <141494354+OlgaAllirina@users.noreply.github.com>
2024-02-10 23:58:54 +0300

    Update main.py
    new file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import json
+import re
+from pprint import pprint
 import requests
 import bs4
 import fake_headers
 
-# Определяем список ключевых слов:
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-URL = 'https://habr.com/ru/articles/'
+URL = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
 
 
 def gen_headers():
@@ -19,30 +20,42 @@ main_html = response.text
 # создадим объект для хранения наших данных с полным списком статей
 main_page = bs4.BeautifulSoup(main_html, "lxml")
 # создадим переменную, в которой будут храниться все статьи
-article_list_tag = main_page.find("div", class_="tm-articles-list")
-article_body_tags = article_list_tag.find_all("article")
-
-for article_tag in article_body_tags:
-    h2_tag = article_tag.find("h2", class_="tm-title tm-title_h2") # заголовок
-    a_tag = article_tag.find('a') # относительная ссылка
-    time_tag = article_tag.find("time") # дата
-    # извлекаем информацию
-    pub_time = time_tag["datetime"]
-    link_relative = a_tag["href"]
-    link_absolute = f"https://habr.com{link_relative}"
-    header = time_tag.text.strip()
-
-    # вытащим текст
-    response = requests.get(link_absolute, headers=gen_headers())
-    article_html = response.text
-    article_page = bs4.BeautifulSoup(article_html, "lxml")
-    article_body_tag = article_tag.find(class_="article-formatted-body article-formatted-body "
-                                               "article-formatted-body_version-2")
-    article_text = article_body_tag.text.strip()
-
-    for search_word in KEYWORDS:
-        if (search_word in header) or (search_word in article_text):
-            print(f'Дата: {pub_time} - Заголовок: {header} - Ссылка: {link_absolute}')
+vac_list_tag = main_page.find("div", id="a11y-main-content")
+vac_body_tags = vac_list_tag.find_all("div", class_="serp-item")
+vacancies = []
 
 
+for vac_tag in vac_body_tags:
+    span_tag = vac_tag.find("span", class_="serp-item__title")
+    # название вакансии
+    name_vac = span_tag.text.strip()
+    # относительная ссылка на вакансию
+    link_vac = vac_tag.find("a")["href"]
+    link_true_vac = f"https://spb.hh.ru{link_vac}"
+    # зарплата
+    span_tag_salary = vac_tag.find("span", class_="bloko-header-section-2")
+    span_tag_salary = (re.sub('\u202f', '', span_tag_salary.text.strip())
+                       if span_tag_salary else "зарплата не указана")
+    # организация
+    organization_vac = vac_tag.find("a", class_="bloko-link bloko-link_kind-tertiary")
+    organization_vac = (re.sub('\xa0', ' ', organization_vac.text.strip())
+                        if organization_vac else "не указана")
+    # город
+    country_org = vac_tag.find("div", attrs={"data-qa": "vacancy-serp__vacancy-address"})
+    country_org = (re.findall(r'^\w+-*\w*\b', country_org.text.strip())[0] if country_org else "город не указан")
+
+    vacancies.append(
+        {
+            "name": name_vac,  # название вакансии
+            "link": link_true_vac,  # ссылка на вакансию
+            "salary": span_tag_salary,  # зарплата
+            "organization": organization_vac,  # организация
+            "address": country_org,  # город
+        }
+    )
+
+pprint(vacancies, width=100, sort_dicts=False)
+
+with open("vacancies.json", "w", encoding="utf-8") as file:
+    json.dump(vacancies, file, indent=4, ensure_ascii=False)
 
